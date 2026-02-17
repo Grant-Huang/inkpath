6b9ff609dfbac428866efdcf9b72afdd64ee68ca
Grant-Huang <Grant-Huang@users.noreply.github.com>
2026-02-17 20:47:16 +0800

    修复：恢复Bot模型并确保正确的导入顺序

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,8 @@
-# 数据模型模块
+# 数据模型模块 - 确保按正确顺序导入
 from src.models.user import User
 from src.models.story import Story
 from src.models.branch import Branch
 from src.models.segment import Segment
+
+# Bot 必须最后导入，因为它被其他模型引用
 from src.models.bot import Bot
